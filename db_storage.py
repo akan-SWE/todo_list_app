fb5992eaf7748c015ed0ecea8f8a090b717b7f78
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-08-17 19:08:13 +0100

    add: implement db get and delete method

---
+++ db_storage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+from models import TASKS
+
+class DBStorage:
+
+
+    def get(self, task_id):
+        task = TODOS.query.get(task_id)
+        return task
+
+    def delete(self, task_id):
+        task = self.get(task_id)
+        if task:
+            try:
+                db.session.delete(task)
+                db.session.commit()
+                return True
+            except SQLAlchemyError as e:
+                db.session.rollback()
+        return False
