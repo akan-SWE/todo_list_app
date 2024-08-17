299ef01de7fbb46ac8172dd275784d7e91283640
Ekwensu <chimasamuel6@gmail.com>
2024-08-17 18:58:04 +0100

    Added Todo model

--- models.py
+++ models.py
@@ -1,18 +1,18 @@
+import uuid
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Todo(db.Model):
-	__tablename__ = 'todos'
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
 
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(100),  nullables=False)
-	content = db.Column(db.String(200), nullable=False
-	completed = db.Column(db.Boolean, default=False)
-	created_at = db.Column(db.DateTime, default=datetime.utcnow)
-	updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(200), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
-
-	def __ref__(self):
-		return f"<Todos {self.title}>"
+    def __repr__(self):
+        return f"<Task {self.title}>"
